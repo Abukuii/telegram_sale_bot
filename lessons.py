4e77598b5b171d6527ce36c22e2bbf96bb6750f3
Abukuii <160826735+Abukuii@users.noreply.github.com>
2026-04-13 14:54:03 +0500

    Update lessons.py
    update lessons

--- lessons.py
+++ lessons.py
@@ -2,25 +2,25 @@ LESSONS = {
 
 "lesson1": {
 "name": "F.I.SH",
-"price": "49 000 so'm",
+"price": "10 000 so'm",
 "file": "./darsliklar/F.ISH.xlsx"
 },
 
 "lesson2": {
 "name": "JSHSHR TEL",
-"price": "29 000 so'm",
+"price": "20 000 so'm",
 "file": "./darsliklar/JSHSHR Tel.xlsx"
 },
 
 "lesson3": {
 "name": "Krill Lotin",
-"price": "29 000 so'm",
+"price": "10 000 so'm",
 "file": "./darsliklar/Krill Lotin.xlsm"
 },
 
 "lesson4": {
 "name": "YOSH ORALIG'I",
-"price": "39 000 so'm",
+"price": "10 000 so'm",
 "file": "./darsliklar/YOSH ORALIGI.xlsx"
 }
 
